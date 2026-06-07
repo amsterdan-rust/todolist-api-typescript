64e1da2698da2e79edc1e8d5dcc454da83646e53
Amsterdan Vasconcelos <amsterdan.rust@gmail.com>
2026-06-07 10:19:51 -0300

    refactor: remove fake auth middleware

--- src/features/todo/task/infra/http/hono/routes/tasks.routes.ts
+++ src/features/todo/task/infra/http/hono/routes/tasks.routes.ts
@@ -1,7 +1,7 @@
 import type { OpenAPIHono } from "@hono/zod-openapi";
 
 import type { AppContainer } from "@app/container";
-import type { AuthVariables } from "@app/http/hono/middlewares/fake-auth.middleware";
+import type { AuthVariables } from "/auth/infra/better-auth/better-auth.middleware";
 import { createTaskRoute } from "./create-task/create-task.route";
 import { getTaskRoute } from "./get-task/get-task.route";
 import { listTasksRoute } from "./list-tasks/list-tasks.route";
